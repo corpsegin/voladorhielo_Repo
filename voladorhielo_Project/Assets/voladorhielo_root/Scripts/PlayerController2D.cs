670ae53ec73674cbfdd49bb21976c2853b4c3d0b
corpsegin <eidenzuluaga64@gmail.com>
2026-01-23 17:56:49 +0100

    muchascosas

--- voladorhielo_Project/Assets/voladorhielo_root/Scripts/PlayerController2D.cs
+++ voladorhielo_Project/Assets/voladorhielo_root/Scripts/PlayerController2D.cs
@@ -19,15 +19,34 @@ public class PlayerController2D : MonoBehaviour
     [SerializeField] Transform shootPosition; //Ref al punto de disparo
     [SerializeField] GameObject projectile; //Ref al prefab del proyectil
 
+    [Header("Crouch Configuration")]
+    [SerializeField] Transform headCheck;
+    [SerializeField] float headCheckRadius = 0.1f;
+    [SerializeField] LayerMask detectorLayer;
+
     //Variables de referencia privadas
     Rigidbody2D playerRb; //Referencia al rigidbody del player
+    BoxCollider2D playerCollider;
     Animator anim; //Referencia al controlador de animaciones del player
     PlayerInput input; //Referencia al cerebro de inputs del player
     Vector2 moveInput; //Referencia al valor pulsado de las teclas de movimiento
+    Vector2 normalColliderSize;
+    Vector2 normalColliderOffset;
     bool canAttack; //Comprobador para determinar si se puede atacar
+    bool isCrouching;
+    Dialogue currentDialogue;
+    GameObject currentInteractable;
+
 
     [Header("Death configuration")]
     [SerializeField] Transform RespawnPoint;
+
+    [Header("taken objects")]
+    [SerializeField] int stairs = 0;
+    [SerializeField] int key = 0;
+    [SerializeField] int neededstairs = 0;
+    [SerializeField] int neededkey = 0;
+
     #endregion
 
     private void Awake()
@@ -36,11 +55,16 @@ public class PlayerController2D : MonoBehaviour
         anim = GetComponent<Animator>();
         input = GetComponent<PlayerInput>();
         canAttack = true;
+        playerCollider = GetComponent<BoxCollider2D>();
+        normalColliderSize = playerCollider.size;
+        normalColliderOffset = playerCollider.offset;
+       
+
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -62,6 +86,11 @@ public class PlayerController2D : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.TryGetComponent(out Dialogue dialogue))
+        {
+            currentDialogue = dialogue;
+        }
+
         if (collision.gameObject.CompareTag("Detectors"))
         {
             Respawn();
@@ -71,13 +100,35 @@ public class PlayerController2D : MonoBehaviour
         {
             RespawnPoint = collision.transform;
         }
+
+        if (collision.CompareTag("Key") || collision.CompareTag("Stair"))
+        {
+            currentInteractable = collision.gameObject;
+        }
+
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Dialogue dialogue))
+        {
+            if (currentDialogue == dialogue)
+                currentDialogue = null;
+        }
+
+        if (collision.gameObject == currentInteractable)
+        {
+            currentInteractable = null;
+        }
+
+    }
+
 
     void Respawn()
     {
         playerRb.position = RespawnPoint.position;
     }
-   
+
 
     void Movement()
     {
@@ -110,6 +161,31 @@ public class PlayerController2D : MonoBehaviour
         yield return null;
     }
 
+    void Crouch()
+    {
+        isCrouching = true;
+
+        playerCollider.size = new Vector2( normalColliderSize.x, normalColliderSize.y / 3f); 
+
+        playerCollider.offset = new Vector2(
+            normalColliderOffset.x, normalColliderOffset.y - normalColliderSize.y / 3f);
+    }
+
+    void StandUp()
+    {
+        playerCollider.size = normalColliderSize;
+        playerCollider.offset = normalColliderOffset;
+        isCrouching = false;
+    }
+
+    bool CanStandUp()
+    {
+        return !Physics2D.OverlapCircle(
+            headCheck.position,
+            headCheckRadius,
+            detectorLayer);
+    }
+
     void AnimationManagement()
     {
         //Gestión del cambio de animaciones: idle-jump-walk
@@ -123,7 +199,7 @@ public class PlayerController2D : MonoBehaviour
         //Llamar a un Instantiate de prefab de proyectil
         GameObject actualProjectile = Instantiate(projectile, shootPosition.position, Quaternion.identity);
         //Bullet bulletScript = actualProjectile.GetComponent<Bullet>();
-       // bulletScript.isFacingRight = isFacingRight;
+        // bulletScript.isFacingRight = isFacingRight;
     }
 
     #region Input Methods
@@ -139,11 +215,47 @@ public class PlayerController2D : MonoBehaviour
     {
         if (context.performed && isGrounded && canAttack) StartCoroutine(Attack());
     }
-    
+
     public void OnShoot(InputAction.CallbackContext context)
     {
-        if (context.performed)ShootMagic();
+        if (context.performed) ShootMagic();
     }
 
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Crouch();
+        }
+
+        if (context.canceled && CanStandUp())
+        {
+            StandUp();
+        }
+
+
+        
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed && currentDialogue != null)
+        {
+            currentDialogue.Interact();
+        }
+
+        if (!context.performed || currentInteractable == null)
+            return;
+
+        if (currentInteractable.CompareTag("Key"))
+        {
+            key++;
+        }
+        else if (currentInteractable.CompareTag("Stair"))
+        {
+            stairs++;
+        }
+
+    }
     #endregion
 }
