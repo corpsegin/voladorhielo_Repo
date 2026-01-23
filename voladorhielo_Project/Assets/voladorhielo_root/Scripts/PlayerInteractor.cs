d338b145bc6ab203876c985613a0e0c4734ccd43
corpsegin <eidenzuluaga64@gmail.com>
2026-01-23 18:58:41 +0100

    Reapply "muchascosas"
    This reverts commit c0040bedc4b89276d82f230b4aff10b3eb23f251.

--- voladorhielo_Project/Assets/voladorhielo_root/Scripts/PlayerInteractor.cs
+++ voladorhielo_Project/Assets/voladorhielo_root/Scripts/PlayerInteractor.cs
@@ -1,20 +1,16 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    [SerializeField] int loseHealth = 10;
-    [SerializeField] Transform RespawnPoint;
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("BadPick"))
-            //acceso al singleton GameManager
-            GameManager.Instance.PlayerHealth -= loseHealth;
-        collision.gameObject.SetActive(false);
+  
 
-        
-    }
+   
+  
 
+
+   
 
 
 }
