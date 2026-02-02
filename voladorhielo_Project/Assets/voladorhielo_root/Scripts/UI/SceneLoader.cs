98a923030681ab2e55bf959efd1b095685a56b2f
noes-elena <elenamunozpardo6@gmail.com>
2026-02-02 18:13:51 +0100

    OptionsMenu

--- voladorhielo_Project/Assets/voladorhielo_root/Scripts/UI/SceneLoader.cs
+++ voladorhielo_Project/Assets/voladorhielo_root/Scripts/UI/SceneLoader.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public GameObject optionsPanel;
+    public GameObject creditsPanel;
     public void LoadScene(int sceneToLoad)
     {
         SceneManager.LoadScene(sceneToLoad);
@@ -12,5 +14,17 @@ public class SceneLoader : MonoBehaviour
     {
         Debug.Log("Has cerrado el juego");
         Application.Quit();
+    }
+
+    public void StartCredits()
+    {
+        creditsPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+    }
+
+    public void CloseCredits()
+    {
+        creditsPanel.SetActive(false);
+        optionsPanel.SetActive(true);
     }
 }
