076e5776acfaccd29792cd1f9057cea0c71693c5
corpsegin <eidenzuluaga64@gmail.com>
2026-02-10 22:56:48 +0100

    nose

---
+++ voladorhielo_Project/Assets/voladorhielo_root/Scripts/Managers/CinematicsManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class IntroVideoSimple : MonoBehaviour
+{
+    [SerializeField] private int nextSceneIndex = 1; // número de escena
+
+    private VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        videoPlayer.renderMode = VideoRenderMode.CameraNearPlane;
+        videoPlayer.targetCamera = Camera.main;
+        videoPlayer.aspectRatio = VideoAspectRatio.FitVertically;
+
+        videoPlayer.loopPointReached += OnVideoFinished;
+        videoPlayer.Play();
+    }
+
+    void Update()
+    {
+        // Opcional: saltar video con cualquier tecla
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+
+    private void OnVideoFinished(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
+
